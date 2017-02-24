586db3bc6a1b1d49ddb13b0a5a4226400f031fed
Kevin Deisz <kevin.deisz@gmail.com>
2017-02-24 17:14:36 -0500

    Build in ::to_json substitutions

--- lib/cogito.rb
+++ lib/cogito.rb
@@ -1,2 +1,20 @@
 require 'cogito/cogito'
 require 'cogito/version'
+
+module Cogito
+  class << self
+    def to_json(str, subs = {})
+      convert_to_json(substitute(str, subs))
+    end
+
+    private
+
+    def substitute(str, subs)
+      substituted = str.dup
+      subs.each do |key, value|
+        substituted.gsub!("${#{key}}", value)
+      end
+      substituted
+    end
+  end
+end
